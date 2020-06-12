c09a5eb502d2c158f6803f7d766eb97018068a13
530525 <530525@nd.com>
2020-06-12 18:36:28 +0800

    【A】新增密码长度及强度规则校验

---
+++ i18n/src/main/java/com/wondernect/elements/i18n/validator/ESPassword.java
@@ -0,0 +1,35 @@
+package com.wondernect.elements.i18n.validator;
+
+import javax.validation.Constraint;
+import javax.validation.Payload;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * Copyright (C), 2017-2020, wondernect.com
+ * FileName: ESPassword
+ * Author: chenxun
+ * Date: 2020/6/12 17:40
+ * Description: 自定义密码注解
+ */
+@Target({ElementType.METHOD,ElementType.FIELD})
+@Retention(RetentionPolicy.RUNTIME)
+@Constraint(validatedBy = ESPasswordValidator.class)
+public @interface ESPassword {
+
+    int min() default 6;
+
+    int max() default 20;
+
+    boolean strong() default true;
+
+    String passwordRegexPattern() default "^(?![A-Z]*$)(?![a-z]*$)(?![0-9]*$)(?![^a-zA-Z0-9]*$)\\S+$";
+
+    String message() default "密码为空或长度规则有误";
+
+    Class<?>[] groups() default {};
+
+    Class<? extends Payload>[] payload() default {};
+}
---
+++ i18n/src/main/java/com/wondernect/elements/i18n/validator/ESPasswordValidator.java
@@ -0,0 +1,58 @@
+package com.wondernect.elements.i18n.validator;
+
+import com.wondernect.elements.common.utils.ESRegexUtils;
+import com.wondernect.elements.common.utils.ESStringUtils;
+
+import javax.validation.ConstraintValidator;
+import javax.validation.ConstraintValidatorContext;
+
+/**
+ * Copyright (C), 2017-2020, wondernect.com
+ * FileName: ESPasswordValidator
+ * Author: chenxun
+ * Date: 2020/6/12 17:47
+ * Description: 密码验证器
+ */
+public class ESPasswordValidator implements ConstraintValidator<ESPassword, String> {
+
+    private int min = 6;
+
+    private int max = 20;
+
+    private boolean strong = true;
+
+    private String passwordRegexPattern = "^(?![A-Z]*$)(?![a-z]*$)(?![0-9]*$)(?![^a-zA-Z0-9]*$)\\S+$";
+
+    @Override
+    public boolean isValid(String s, ConstraintValidatorContext constraintValidatorContext) {
+        if (ESStringUtils.isBlank(s)) {
+            constraintValidatorContext.disableDefaultConstraintViolation();
+            constraintValidatorContext.buildConstraintViolationWithTemplate("密码不能为空");
+            return false;
+        }
+        if (min <= 0 || max <= 0 || max < min) {
+            constraintValidatorContext.disableDefaultConstraintViolation();
+            constraintValidatorContext.buildConstraintViolationWithTemplate("密码长度设置最大最小值非法");
+            return false;
+        }
+        if (s.length() < min || s.length() > max) {
+            constraintValidatorContext.disableDefaultConstraintViolation();
+            constraintValidatorContext.buildConstraintViolationWithTemplate("密码长度必须在" + min + "与" + max + "之间");
+            return false;
+        }
+        if (strong && !ESRegexUtils.match(passwordRegexPattern, s)) {
+            constraintValidatorContext.disableDefaultConstraintViolation();
+            constraintValidatorContext.buildConstraintViolationWithTemplate("密码强度不符合规则要求,请检查修改后重试");
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public void initialize(ESPassword constraintAnnotation) {
+        min = constraintAnnotation.min();
+        max = constraintAnnotation.max();
+        strong = constraintAnnotation.strong();
+        passwordRegexPattern = constraintAnnotation.passwordRegexPattern();
+    }
+}
