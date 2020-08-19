dc8b6507ec0b750b7d9b7d773ece30be28150f53
530525 <530525@nd.com>
2020-08-19 10:54:37 +0800

    【m】ssl修改更新

--- common/src/main/java/com/wondernect/elements/common/utils/ESJSONObjectUtils.java
+++ common/src/main/java/com/wondernect/elements/common/utils/ESJSONObjectUtils.java
@@ -6,12 +6,16 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import javax.net.ssl.*;
 import java.io.*;
-import java.net.MalformedURLException;
 import java.net.URL;
 import java.net.URLConnection;
 import java.nio.charset.StandardCharsets;
-import java.util.HashMap;
+import java.security.KeyManagementException;
+import java.security.NoSuchAlgorithmException;
+import java.security.SecureRandom;
+import java.security.cert.CertificateException;
+import java.security.cert.X509Certificate;
 import java.util.List;
 import java.util.Map;
 
@@ -80,6 +84,50 @@ public final class ESJSONObjectUtils {
         return null;
     }
 
+    private static class miTM implements TrustManager,X509TrustManager {
+        public X509Certificate[] getAcceptedIssuers() {
+            return null;
+        }
+
+        public boolean isServerTrusted(X509Certificate[] certs) {
+            return true;
+        }
+
+        public boolean isClientTrusted(X509Certificate[] certs) {
+            return true;
+        }
+
+        public void checkServerTrusted(X509Certificate[] certs, String authType) throws CertificateException {
+
+        }
+
+        public void checkClientTrusted(X509Certificate[] certs, String authType) throws CertificateException {
+
+        }
+    }
+
+    private static void trustAllHttpsCertificates() throws Exception {
+        TrustManager[] trustAllCerts = new TrustManager[1];
+        TrustManager tm = new miTM();
+        trustAllCerts[0] = tm;
+        SSLContext sc = SSLContext.getInstance("SSL");
+        sc.init(null, trustAllCerts, null);
+        HttpsURLConnection.setDefaultSSLSocketFactory(sc.getSocketFactory());
+    }
+
+    /**
+     * 忽略HTTPS请求的SSL证书，必须在openConnection之前调用
+     */
+    private static void ignoreSsl() throws Exception{
+        HostnameVerifier hv = new HostnameVerifier() {
+            public boolean verify(String urlHostName, SSLSession session) {
+                return true;
+            }
+        };
+        trustAllHttpsCertificates();
+        HttpsURLConnection.setDefaultHostnameVerifier(hv);
+    }
+
     /**
      * 通过网络访问json并读取文件
      *
@@ -89,6 +137,7 @@ public final class ESJSONObjectUtils {
         StringBuilder json = new StringBuilder();
         BufferedReader bufferedReader = null;
         try {
+            ignoreSsl();	//在openConnection前调用该方法
             URLConnection urlConnection = new URL(url).openConnection();
             bufferedReader = new BufferedReader(new InputStreamReader(urlConnection.getInputStream(), StandardCharsets.UTF_8));
             String inputLine;
@@ -96,7 +145,7 @@ public final class ESJSONObjectUtils {
                 json.append(inputLine);
             }
             bufferedReader.close();
-        } catch (IOException e) {
+        } catch (Exception e) {
             logger.error("load json string from url failed", e);
         } finally {
             if (bufferedReader != null) {
@@ -142,16 +191,17 @@ public final class ESJSONObjectUtils {
     }
 
     public static void main(String[] args) {
-        Map<String, Double> map = new HashMap<>();
-        map.put("5.0", 10.0);
-        map.put("10.0", 20.0);
-        map.put("30.0", 50.0);
-        String json = jsonObjectToJsonString(map);
-        System.out.println(json);
-        Map<String, Double> jsonMap = jsonStringToMapClassObject(json, Double.class);
-        System.out.println(jsonMap);
-        System.out.println(jsonMap.keySet());
-        System.out.println(jsonMap.values());
+        // Map<String, Double> map = new HashMap<>();
+        // map.put("5.0", 10.0);
+        // map.put("10.0", 20.0);
+        // map.put("30.0", 50.0);
+        // String json = jsonObjectToJsonString(map);
+        // System.out.println(json);
+        // Map<String, Double> jsonMap = jsonStringToMapClassObject(json, Double.class);
+        // System.out.println(jsonMap);
+        // System.out.println(jsonMap.keySet());
+        // System.out.println(jsonMap.values());
         System.out.println(loadJsonStringFromLocalPath("F:\\aa.json"));
+        System.out.println(loadJsonStringFromUrl("https://service.cela.gov.cn/system/studyinfo/file/url?signature=eyJzdWIiOiJPUEVOIiwiaXNzIjoiOGE5OThhMTU3MjcyODYwYTAxNzI3MmJlMzJmMjAwMjciLCJleHAiOjE1OTc4MTA4MzB9.bHjASnTEdIlhLSM-dCEl4h6QdtytIYVdQXVtIR4LPP4&token=3daa530bd9191b1c782148dc3de67e64&file_id=8a998a1474024c9801740250155f002b"));
     }
 }
