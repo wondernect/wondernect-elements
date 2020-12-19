b0e8371589097c3df806707a2334644f68154d8d
530525 <530525@nd.com>
2020-12-19 18:26:44 +0800

    【m】删除boot application data jpa依赖&备份log4j日志配置文件

--- boot-application/src/main/java/com/wondernect/elements/boot/application/WondernectBootApplication.java
+++ boot-application/src/main/java/com/wondernect/elements/boot/application/WondernectBootApplication.java
@@ -6,11 +6,9 @@ import com.wondernect.elements.boot.application.event.WondernectBootEventType;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.ComponentScans;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 /**
  * Copyright (C), 2020, wondernect.com
@@ -21,12 +19,6 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
  */
 @ComponentScans({
         @ComponentScan(basePackages = "com.wondernect.*")
-})
-@EntityScan(basePackages = {
-        "com.wondernect.*"
-})
-@EnableJpaRepositories(basePackages = {
-        "com.wondernect.*"
 })
 @SpringBootApplication
 public abstract class WondernectBootApplication implements CommandLineRunner {
